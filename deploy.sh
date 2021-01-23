d860a285f73a1e71b6267673ea491173696b9899
Yan Pereira <yan.pereira@ilegra.com>
2021-01-23 19:03:02 -0300

    Update docker-compose file, deploy and destroy scripts

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 ./gradlew build
 
-docker build -t voting-session-manager:latest
+docker build -t voting-session-manager:latest .
 
 docker-compose up -d
--- destroy.sh
+++ destroy.sh
@@ -1,2 +1,11 @@
 #! /bin/bash -e
 
+app_container_id=$(docker ps --filter name=voting-session-manager --quiet)
+mongo_container_id=$(docker ps --filter name=mongo --quiet)
+
+app_image_id=$(docker image ls --filter reference=voting-session-manager --quiet)
+
+docker stop $app_container_id && docker rm $app_container_id
+docker stop $mongo_container_id && docker rm $mongo_container_id
+
+docker rmi -f $app_image_id
