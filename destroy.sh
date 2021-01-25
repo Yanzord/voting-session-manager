06af5f9cb5303954496325c986c6f357594243cc
Yan Pereira <yan.pereira@ilegra.com>
2021-01-24 21:19:21 -0300

    Update destroy script to delete mongo image

--- destroy.sh
+++ destroy.sh
@@ -4,8 +4,10 @@ app_container_id=$(docker ps --filter name=voting-session-manager --quiet)
 mongo_container_id=$(docker ps --filter name=mongo --quiet)
 
 app_image_id=$(docker image ls --filter reference=voting-session-manager --quiet)
+mongo_image_id=$(docker image ls --filter reference=mongo --quiet)
 
 docker stop $app_container_id && docker rm $app_container_id
 docker stop $mongo_container_id && docker rm $mongo_container_id
 
 docker rmi -f $app_image_id
+docker rmi -f $mongo_image_id
